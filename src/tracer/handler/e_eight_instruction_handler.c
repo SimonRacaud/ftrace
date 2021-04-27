e002a597b43abc97cfadaf0e124745bdb780c21d
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-04-27 15:26:54 +0200

    - Add 0xe8 manage

---
+++ src/tracer/handler/e_eight_instruction_handler.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2021
+** e_eight_instruction_handler.c
+** File description:
+** e_eight_instruction_handler
+*/
+
+#include "app.h"
+#include "libc.h"
+
+extern const char ENTERING_FUNCTION[];
+
+int e_eight_instruction_handler(tracer_t *tracer, registers_t *registers)
+{
+    printf("HERE 0xe8??\n");
+    char *name = NULL;
+    int ret = 0;
+
+    ptrace(PTRACE_SINGLESTEP, tracer->child_pid, NULL, NULL);
+    wait4(tracer->child_pid, &ret, 0, NULL);
+    ptrace(PTRACE_GETREGS, tracer->child_pid, 0, registers->rip);
+    if (ptrace(PTRACE_PEEKDATA, tracer->child_pid, registers->rip, NULL) == -1)
+        return false;
+    // name = get_func_name(tracer->child_pid, registers->rip);
+    name = strdup("NOT ALREADY DEV");
+    if (!name)
+        return false;
+    fprintf(stderr, ENTERING_FUNCTION, name, registers->rip);
+    return stack_push(&tracer->call_names, name);
+}
