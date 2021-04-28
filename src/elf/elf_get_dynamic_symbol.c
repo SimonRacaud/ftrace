14beb16df46a12c10476d15873c0d390dccfa708
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-04-28 13:23:03 +0200

    - Fix get_dynamic_name

--- src/elf/elf_get_dynamic_symbol.c
+++ src/elf/elf_get_dynamic_symbol.c
@@ -24,26 +24,35 @@ static uint64_t get_pl_inf(Elf *elf, size_t idx_relaplt)
     return section_header.sh_addr + section_header.sh_entsize * idx_relaplt;
 }
 
+static void move_to_rela_plt(Elf *elf,
+Elf_Scn **section, GElf_Shdr *section_header)
+{
+    size_t tmp = 0;
+
+    elf_getshdrstrndx(elf, &tmp);
+    while ((*section = elf_nextscn(elf, *section)) != NULL) {
+        gelf_getshdr(*section, section_header);
+        if (section_header->sh_type == SHT_RELA &&
+        strcmp(".rela.plt", elf_strptr(elf, tmp, section_header->sh_name)) == 0)
+            break;
+    }
+}
+
 char *get_dynamic_name(Elf *elf,
 size_t sh_link_dyntab, Elf_Data *data_dynsim, uint64_t addr)
 {
-    size_t tmp = 0;
     char *name = NULL;
     Elf_Scn *section = NULL;
     GElf_Shdr section_header = {0};
     GElf_Rela rela = {0};
     GElf_Sym sym = {0};
     Elf_Data *data_rela = NULL;
+    size_t rela_size = 0;
 
-    elf_getshdrstrndx(elf, &tmp);
-    while ((section = elf_nextscn(elf, section)) != NULL) {
-        gelf_getshdr(section, &section_header);
-        if (section_header.sh_type == SHT_RELA &&
-        strcmp(".rela.plt", elf_strptr(elf, tmp, section_header.sh_name)) == 0)
-            break;
-    }
+    move_to_rela_plt(elf, &section, &section_header);
     data_rela = elf_getdata(section, NULL);
-    for (size_t i = 0; i < section_header.sh_size / section_header.sh_entsize; i++) {
+    rela_size = section_header.sh_size / section_header.sh_entsize;
+    for (size_t i = 0; i < rela_size; i++) {
         gelf_getrela(data_rela, i, &rela);
         gelf_getsym(data_dynsim, GELF_R_SYM(rela.r_info), &sym);
         if (addr == get_pl_inf(elf, i)) {
