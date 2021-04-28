449988d26bd7965676a4ca0b97f98a71ca41a6a8
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-04-28 13:34:35 +0200

    - Fix src/elf/elf_parser_get_symbol_name.c (PS. END OF GET_DYNAMIC_SYMBOL)

--- src/elf/elf_parser_get_symbol_name.c
+++ src/elf/elf_parser_get_symbol_name.c
@@ -10,6 +10,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+static void move_to_section(Elf *elf,
+Elf_Scn **section, GElf_Shdr *section_header, Elf64_Word type)
+{
+    while ((*section = elf_nextscn(elf, *section)) != NULL) {
+        gelf_getshdr(*section, section_header);
+        if (section_header->sh_type == type)
+            break;
+    }
+}
+
 static size_t get_nb_plt_sym(Elf *elf)
 {
     size_t tmp = 0;
@@ -50,38 +60,41 @@ static char *elf_parser_find_name(
     return strdup(function_name);
 }
 
+static char *elf_parser_symbol(Elf *elf, uint64_t addr)
+{
+    GElf_Shdr section_header = {0};
+    Elf_Data *data_dynsim = NULL;
+    char *function_name = NULL;
+    size_t sh_link_dyntab = 0;
+    Elf_Scn *section = NULL;
+
+    move_to_section(elf, &section, &section_header, SHT_SYMTAB);
+    function_name = elf_parser_find_name(addr, &section_header, section, elf);
+    section = NULL;
+    if (!function_name) {
+        move_to_section(elf, &section, &section_header, SHT_DYNSYM);
+        sh_link_dyntab = section_header.sh_link;
+        data_dynsim = elf_getdata(section, NULL);
+        function_name = get_dynamic_name(elf,
+        sh_link_dyntab, data_dynsim, addr - (16 * get_nb_plt_sym(elf)));
+        if (!function_name)
+            function_name = get_dynamic_name(elf,
+            sh_link_dyntab, data_dynsim, addr);
+    }
+    return function_name;
+}
+
 char *elf_parser_get_symbol_name(char *elf_filepath, uint64_t addr)
 {
-    size_t sh_link_dyntab = 0;
     int fd = open(elf_filepath, O_RDONLY);
     Elf *elf = NULL;
-    Elf_Scn *section = NULL;
-    GElf_Shdr section_header = {0};
     char *function_name = NULL;
 
     if (fd == -1)
         return NULL;
     elf_version(EV_CURRENT);
     elf = elf_begin(fd, ELF_C_READ, NULL);
-    while ((section = elf_nextscn(elf, section)) != NULL) {
-        gelf_getshdr(section, &section_header);
-        if (section_header.sh_type == SHT_SYMTAB)
-            break;
-    }
-    function_name = elf_parser_find_name(addr, &section_header, section, elf);
-    section = NULL;
-    if (!function_name) {
-        while ((section = elf_nextscn(elf, section)) != NULL) {
-            gelf_getshdr(section, &section_header);
-            if (section_header.sh_type == SHT_DYNSYM)
-                break;
-        }
-        sh_link_dyntab = section_header.sh_link;
-        Elf_Data *data_dynsim = elf_getdata(section, NULL);
-        function_name = get_dynamic_name(elf, sh_link_dyntab, data_dynsim, addr - (16 * get_nb_plt_sym(elf)));
-        if (!function_name)
-            function_name = get_dynamic_name(elf, sh_link_dyntab, data_dynsim, addr);
-    }
+    function_name = elf_parser_symbol(elf, addr);
     elf_end(elf);
     close(fd);
     return function_name;
